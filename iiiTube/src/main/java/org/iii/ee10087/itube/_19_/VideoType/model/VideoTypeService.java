5c7c2f41c42e22865229f4229fdb00c8be551942
kings7788 <34169622+kings7788@users.noreply.github.com>
2018-04-24 17:49:22 +0800

    fix commit

--- iiiTube/src/main/java/org/iii/ee10087/itube/_19_/VideoType/model/VideoTypeService.java
+++
@@ -1,8 +0,0 @@
-package org.iii.ee10087.itube._19_.VideoType.model;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface VideoTypeService {
-	public List<String> getAllVideoType() throws SQLException;
-}
